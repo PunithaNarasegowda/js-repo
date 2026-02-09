ea6c1580a5b491976a7acf49bb87702d31637244
PunithaNarasegowda <10c07punitha.n@gmail.com>
2026-02-10 01:30:43 +0530

    new promises

--- 09_AdvanceOne/promises.js
+++ 09_AdvanceOne/promises.js
@@ -98,5 +98,21 @@ fetch('https://api.github.com/users/hiteshchoudhary')
 })
 .catch((error) => console.log(error))
 
-// promise.all
-// yes this is also available, kuch reading aap b kro.
+// promise.all is basically used to run multiple promises in parallel and wait for all of them to resolve before proceeding. 
+// It takes an array of promises as input and returns a new promise that resolves when all the input promises have resolved, or rejects if any of the input promises reject.
+//  This is useful when you have multiple asynchronous tasks that can be executed concurrently, and you want to wait for all of them to complete before moving on to the next step in your code.
+//for example, if you want to fetch data from multiple APIs at the same time and then process the results together, you can use Promise.all to handle this efficiently.
+const promiseSix = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve("Promise 6 resolved")
+    }, 1000) })
+
+const promiseSeven = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve("Promise 7 resolved")
+    }, 2000) })
+
+Promise.all([promiseSix, promiseSeven])
+.then((responses) => {
+    console.log(responses);
+}).catch((error) => console.log(error))
